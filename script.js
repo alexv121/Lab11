4772481fc21bca6aa537a9a1cc9664db676945e7
Alexandra Vaughn <alexvaughn121@gmail.com>
2015-11-11 10:06:07 -0500

    update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
+$(document).ready(function() {
 //Add a popover trigger for seats that aren't reserved
+var theThing;
 $(".seat:not(.na)").popover({
     html: true, 
 	title: 'Reserve Your Seat',
 	trigger: 'click',
 	placement: 'bottom',
 	content: function() {
+		theThing = this;
 		return $('#seat-form-content').html();
 	}
-});
+}).parent().delegate('button', 'click', function(event) {
+		$(theThing).css({'background-color': 'red'}); 
+	});
+$( "#seat-form-content" ).submit(function( event ) {
+	  alert( "Handler for .submit() called." );
+	  event.preventDefault();
+	});
 
 //Hide the other popovers when you select another
 $('.seat:not(.na)').on('click', function (e) {
     $('.seat:not(.na)').not(this).popover('hide');
 });
-
-$(".seat:not(.na)").on("click",function (e){
-	$("main").popovers("hide");
 });
+
+
 
